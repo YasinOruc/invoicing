d481bf87e43fcca1116f660e2268c5a285abc593
YasinOruc <orucyasin14@gmail.com>
2024-10-24 20:31:29 +0200

    views

--- invoicing_project/invoices/views.py
+++ invoicing_project/invoices/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Invoice
+from .serializers import InvoiceSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
+from django.http import HttpResponse
 
-# Create your views here.
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+    @action(detail=True, methods=['get'])
+    def pdf(self, request, pk=None):
+        invoice = self.get_object()
+        html = render_to_string('invoice_pdf.html', {'invoice': invoice})
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="invoice_{invoice.id}.pdf"'
+        pisa_status = pisa.CreatePDF(html, dest=response)
+        if pisa_status.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
