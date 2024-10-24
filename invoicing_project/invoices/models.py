4a17490b7a14e45b2718b40a56650e7ae9173ae4
YasinOruc <orucyasin14@gmail.com>
2024-10-24 20:29:13 +0200

    installed rest framework

--- invoicing_project/invoices/models.py
+++ invoicing_project/invoices/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Invoice(models.Model):
+    client_name = models.CharField(max_length=255)
+    client_email = models.EmailField()
+    date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"Invoice {self.id} - {self.client_name}"
--- invoicing_project/invoicing_project/settings.py
+++ invoicing_project/invoicing_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'invoices',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
